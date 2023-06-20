47ea792929acc014e41fc46b8892bd861924daed
yooml <ymlll0508@gmail.com>
2023-06-20 22:26:10 +0800

    Fix pub

--- lib/stable-asset/src/lib.rs
+++ lib/stable-asset/src/lib.rs
@@ -31,7 +31,7 @@ mod tests;
 
 pub mod weights;
 
-use crate::traits::StableAsset;
+pub use crate::traits::StableAsset;
 use frame_support::codec::{Decode, Encode};
 use frame_support::dispatch::{DispatchError, DispatchResult};
 use frame_support::ensure;
